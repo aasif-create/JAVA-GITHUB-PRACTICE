18e6c417f58d4534e6c3d27f7ffab2a3afef49dd
aasif-create <aasifmmd12345@gmail.com>
2025-09-07 21:31:44 +0530

    PRACTICING INPUT IN JAVA

--- INPUT.java
+++ INPUT.java
@@ -1,27 +1,24 @@
 import java.util.Scanner;
-public class input{
+public class INPUT{
     public static void main(String[] args)
     {
         Scanner obj =new Scanner(System.in);
         System.out.print("ENTER YOUR NAME :");
         String name=obj.nextLine();
-        obj.nextLine();
         System.out.print("ENTER YOUR ROLL.NO :");
         int rollno=obj.nextInt();
         obj.nextLine();
         System.out.print("ENTER YOUR DEPARTMENT :");
         String dept=obj.nextLine();
-        obj.nextLine();
         System.out.print("ENTER YOUR SECTION :");
         char sec = obj.nextLine().charAt(0);
-        obj.nextLine();
-
 
         //DISPLAYING THE STUDENTS DETAILS
-        System.out.print("STUDENT'S DETAILS :");
-        System.out.print("STUDENT'S NAME :"+name +"\n");
-        System.out.print("STUDENT'S ROLLNO :"+rollno+"\n");
-        System.out.print("STUDENT'S ROLLNO :"+rollno+"\n");
+        System.out.print("STUDENT'S DETAILS : \n");
+        System.out.print("NAME :"+name +"\n");
+        System.out.print("ROLLNO :"+rollno+"\n");
+        System.out.print("DEPARTMENT :"+dept+"\n");
+        System.out.print("SECTION :"+sec+"\n");
         
         
     
