e31158f51df2e09fc519340c087d4af8fbdcd314
aasif-create <aasifmmd12345@gmail.com>
2025-10-04 19:34:22 +0530

    updated

--- BUTTERFLYPATTERN.java
+++ BUTTERFLYPATTERN.java
@@ -2,17 +2,40 @@ public class BUTTERFLYPATTERN
 {
     public static void main(String[] args)
     {
-        int n=5;
+        int n=6;
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=i;j++)
             {
                 System.out.print("*");
             }
-            for(int j=1;j<=n-i;j++)
+            for(int j=1;j<=2*(n-i);j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
             {
                 System.out.print("*");
             }
+
+            System.out.println();
+        }
+        for(int i=n;i>0;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
         }
     }
 }
