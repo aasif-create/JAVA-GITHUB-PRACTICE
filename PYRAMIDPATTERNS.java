b7868647bc9d91d9d441447235cf0376a5d87754
aasif-create <aasifmmd12345@gmail.com>
2025-10-05 23:00:54 +0530

    UPDATED

--- PYRAMIDPATTERNS.java
+++ PYRAMIDPATTERNS.java
@@ -26,6 +26,19 @@ public class PYRAMIDPATTERNS
             System.out.println();
         }
         System.out.println("<--------------------------------------------------------->");
+        System.out.println("SPACE WITH STAR PYRAMID");
+
+        int n2=7;
+        for(int i=n1 ;i>0;i--)
+        {
+            for(int j=1;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("<--------------------------------------------------------->");
+
 
     }
 }
