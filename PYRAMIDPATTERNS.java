f16b70d3ac549eeff9daeb779eeb2faded1a5460
aasif-create <aasifmmd12345@gmail.com>
2025-10-05 19:47:13 +0530

    UPDATED

--- PYRAMIDPATTERNS.java
+++ PYRAMIDPATTERNS.java
@@ -14,6 +14,18 @@ public class PYRAMIDPATTERNS
         }
         System.out.println("<--------------------------------------------------------->");
 
-        System.out.print("RIGHT HALF PYRAMID");
+        System.out.println("INVERTED HALF PYRAMID");
+
+        int n1=7;
+        for(int i=n1 ;i>0;i--)
+        {
+            for(int j=1;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("<--------------------------------------------------------->");
+
     }
 }
