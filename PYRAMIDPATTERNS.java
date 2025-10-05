25e9c583e7b262c8b22eb7760b805998c75fbf46
aasif-create <aasifmmd12345@gmail.com>
2025-10-05 19:44:25 +0530

    UPDATED

--- PYRAMIDPATTERNS.java
+++ PYRAMIDPATTERNS.java
@@ -1,15 +1,19 @@
 public class PYRAMIDPATTERNS
-{
+{       
     public static void main(String[] args)
     {
-        for(int i =0 ;i<=n;i++)
+        System.out.print("LEFT HALF PYRAMID");
+        int n=7;
+        for(int i =1 ;i<=n;i++)
         {
-            for(int j=0;j<=i;j++)
+            for(int j=1;j<i;j++)
             {
                 System.out.print("*");
             }
             System.out.println();
         }
-    }
+        System.out.println("<--------------------------------------------------------->");
 
+        System.out.print("RIGHT HALF PYRAMID");
+    }
 }
