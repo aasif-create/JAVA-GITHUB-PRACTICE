1b8c7fe2cef8d1dbeb401645d9a7449e4799bd2a
aasif-create <aasifmmd12345@gmail.com>
2025-10-22 00:04:56 +0530

    UPDATED

--- CONDITIONS.java
+++ CONDITIONS.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-public class CONDITIONS {
+public class CONDITIONS
+ {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         System.out.print("ENTER YOUR AGE : ");
