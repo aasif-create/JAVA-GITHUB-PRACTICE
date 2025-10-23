0d4ed0b3d7f6955e890c3d8f6aed8b5382c38a08
aasif-create <aasifmmd12345@gmail.com>
2025-10-23 23:43:59 +0530

    UPDATED

--- CONDITIONS.java
+++ CONDITIONS.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
 public class CONDITIONS {public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("ENTER YOUR AGE : ");
-        int age = obj.nextInt();
+        int age = sc.nextInt();
         if (age >= 18)
             System.out.print("YOU ARE ELIGIBLE TO VOTE");
         else
             System.out.print("YOU ARE NOT ELIGIBLE TO VOTE \n");
+
+         System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
     }
 }
