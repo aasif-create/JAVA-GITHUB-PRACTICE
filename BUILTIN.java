928d80f65e7e3da6378f20b693d73c9d16d6642c
aasif-create <aasifmmd12345@gmail.com>
2025-11-15 21:49:40 +0530

    UPDATED

---
+++ BUILTIN.java
@@ -0,0 +1,3 @@
+public class BUILTIN-FUNC {
+    
+}
--- FUNCTIONS.java
+++ FUNCTIONS.java
@@ -7,16 +7,17 @@ public class FUNCTIONS
     }
 public static void main(String[] args)
 {
-    Scanner obj =new Scanner(System.in);
-    FUNCTIONS sc=new FUNCTIONS();
-    System.out.println("ADDITION");
-    System.out.print("ENTER A VALUE : ");
-    int a=obj.nextInt();
-    System.out.print("ENTER B VALUE : ");
-    int b=obj.nextInt();
-    System.out.print("ENTER C VALUE : ");
-    int c=obj.nextInt();
-    sc.Add(a,b,c);
-    obj.close();
+        try (Scanner obj = new Scanner(System.in))
+         {
+            FUNCTIONS sc=new FUNCTIONS();
+            System.out.println("ADDITION");
+            System.out.print("ENTER A VALUE : ");
+            int a=obj.nextInt();
+            System.out.print("ENTER B VALUE : ");
+            int b=obj.nextInt();
+            System.out.print("ENTER C VALUE : ");
+            int c=obj.nextInt();
+            sc.Add(a,b,c);
+        }
 }
 }
