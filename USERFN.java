a1df385604f83850c407997aed217458721e0291
aasif-create <aasifmmd12345@gmail.com>
2025-12-01 19:34:51 +0530

    UPDATED

---
+++ USERFN.java
@@ -0,0 +1,12 @@
+public class USERFN
+{
+    add(int a , int b)
+    {
+      return a+b;
+    }
+public static void main(String[] args)
+{
+    int a=10;int b=90;
+    System.out.println("SUM OF A AND B IS :"+ add(a,b));
+}
+}
