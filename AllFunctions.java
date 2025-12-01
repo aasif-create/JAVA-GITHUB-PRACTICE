dd00b5f88cd99c1cea19410af2ccca43e63363df
aasif-create <aasifmmd12345@gmail.com>
2025-12-01 19:43:59 +0530

    UPDATED

--- AllFunctions.java
+++ AllFunctions.java
@@ -9,3 +9,17 @@ public class AllFunctions {
         int c = a - b;
         System.out.println("Subtraction = " + c);
     }
+public static int getNumber() {
+        return 50;
+    }
+
+    public static void display() {
+        System.out.println("This is a function WITHOUT parameters & WITHOUT return type");
+        System.out.println("Hello, this is a user-defined function!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1. WITH parameters & WITH return type");
+        int sum = add(10, 20);
+        System.out.println("Addition = " + sum);
+        System.out.println("----------------------------------");
