4747a9b125b55f8421d9da4d607cb3aa3bc35995
aasif-create <aasifmmd12345@gmail.com>
2025-12-01 19:43:26 +0530

    UPDATED

---
+++ AllFunctions.java
@@ -0,0 +1,11 @@
+public class AllFunctions {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void subtract(int a, int b) {
+        System.out.println("This is a function WITH parameters & WITHOUT return type");
+        int c = a - b;
+        System.out.println("Subtraction = " + c);
+    }
