4311fbb38545606c5cd54be339c21562959155b8
aasif-create <aasifmmd12345@gmail.com>
2025-12-03 23:48:28 +0530

    UPDATED

--- USERFN.java
+++ USERFN.java
@@ -35,5 +35,53 @@ public static int getNumber() {
         System.out.println("4. WITHOUT parameters & WITHOUT return type");
         display();
         System.out.println("----------------------------------");
+
+        public class CombinedFunctionDemo {
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Cannot divide by zero!");
+        } else {
+            System.out.println("Division Result = " + (a / b));
+        }
+    }
+
+    public static int generateRandomNumber() {
+        return (int)(Math.random() * 100);
+    }
+
+    public static void greet() {
+        System.out.println("Welcome to the Combined Function Calculator!");
+    }
+    public static void fullDemo() {
+
+        System.out.println("1. WITHOUT parameters & WITHOUT return type");
+        greet();
+        System.out.println("--------------------------------");
+
+        System.out.println("2. WITH parameters & WITH return type");
+        int r1 = multiply(5, 6);
+        System.out.println("Multiplication Result = " + r1);
+        System.out.println("--------------------------------");
+
+        System.out.println("3. WITH parameters & WITHOUT return type");
+        divide(20, 4);
+        System.out.println("--------------------------------");
+
+        System.out.println("4. WITHOUT parameters & WITH return type");
+        int randomValue = generateRandomNumber();
+        System.out.println("Random Number Generated = " + randomValue);
+        System.out.println("--------------------------------");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("***** COMBINED FUNCTION DEMO *****");
+        fullDemo();
+    }
+}
     }
 }
