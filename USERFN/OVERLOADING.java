02beb50c98282ecd3f33a5298f17b074f846e763
aasif-create <aasifmmd12345@gmail.com>
2025-12-05 21:26:34 +0530

    UPDATED

--- USERFN/OVERLOADING.java
+++ USERFN/OVERLOADING.java
@@ -1,3 +1,18 @@
-public class OVERLOADING {
-    
+class Demo {
+    public int add(int a, int b) {
+        System.out.println("add(int a, int b) called");
+        return a + b;
+    }
+    public int add(int a, int b, int c) {
+        System.out.println("add(int a, int b, int c) called");
+        return a + b + c;
+    }
+}
+public class OVERLOADING
+{
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+        System.out.println("Result (2 numbers): " + obj.add(5, 10));
+        System.out.println("Result (3 numbers): " + obj.add(5, 10, 15));
+    }
 }
