ad548c495d8963099165c91d2bdeb4cf5eb6e17e
aasif-create <aasifmmd12345@gmail.com>
2025-12-13 23:09:52 +0530

    UPDATED

--- OOPS/ABSTRACTION.java
+++ OOPS/ABSTRACTION.java
@@ -1,5 +1,24 @@
-package OOPS;
+abstract class Bank {
+
+    abstract void withdraw();
+}
+
+class ATM extends Bank {
+
+    void withdraw() {
+        System.out.println("Withdraw option shown to the user");
+        System.out.println("Internal bank processing is hidden");
+    }
+}
 
 public class ABSTRACTION {
-    
+
+    public static void main(String[] args) {
+
+        System.out.println("Creating ATM object using Bank reference");
+        Bank b = new ATM();
+
+        System.out.println("Calling withdraw method");
+        b.withdraw();
+    }
 }
