54328d8f77b038f35b49c140d40d813470f1e857
aasif-create <aasifmmd12345@gmail.com>
2025-12-13 23:08:57 +0530

    UPDATED

--- OOPS/ENCAPSULATION.java
+++ OOPS/ENCAPSULATION.java
@@ -1,5 +1,29 @@
-package OOPS;
+class BankAccount {
+
+    private int balance;
+
+    public void setBalance(int amount) {
+        System.out.println("Setting balance using setter method");
+        balance = amount;
+    }
+
+    public int getBalance() {
+        System.out.println("Getting balance using getter method");
+        return balance;
+    }
+}
 
 public class ENCAPSULATION {
-    
+
+    public static void main(String[] args) {
+
+        System.out.println("Creating BankAccount object");
+        BankAccount acc = new BankAccount();
+
+        System.out.println("Trying to set balance directly is not allowed");
+        acc.setBalance(5000);
+
+        System.out.println("Accessing balance through getter method");
+        System.out.println("Account Balance = " + acc.getBalance());
+    }
 }
